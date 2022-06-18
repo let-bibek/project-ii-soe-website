964f714c520b8788f9cdcf640be2b1b2a2ab88fa
Bibek Shrestha <kumarbibek094@gmail.com>
2022-06-18 16:31:48 +0545

    Add files via upload

---
+++ adminstats/db.php
@@ -0,0 +1,8 @@
+<?php
+$con=mysqli_connect("localhost","epiz_27617982","7RyfVgOIpth3j","epiz_27617982_blog_admin_db");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+ ?>
